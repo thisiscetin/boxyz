7437b4e73bdddb54a688a1c97029c974fd1068e1
Mehmet Cetin <mcetin.cm@gmail.com>
2022-04-11 20:53:02 +0000

    contract folder

---
+++ contract/truffle-config.js
@@ -0,0 +1,28 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+
+const mnemonic =
+  process.env.MNEMONIC || fs.readFileSync('.secret').toString().trim();
+
+module.exports = {
+  networks: {
+    pluto: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, 'http://10.10.10.113:8545');
+      },
+      network_id: 1,
+    },
+  },
+  mocha: {},
+  compilers: {
+    solc: {
+      version: '^0.8.1',
+      settings: {
+        optimizer: {
+          enabled: false,
+          runs: 200,
+        },
+      },
+    },
+  },
+}; 

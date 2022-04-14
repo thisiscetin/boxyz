d16c746bc249d82ed75ff0211d542efad67cf9f3
Mehmet Cetin <mcetin.cm@gmail.com>
2022-04-14 15:56:30 +0000

    adds client router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+root.render(
   <React.StrictMode>
-    <h1>boxyz</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <>
+      <h1>boxyz</h1>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </>
+  </React.StrictMode>
 );
